11f312d6196a9858dda8514945df986831018c17
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2021-04-02 18:23:43 -0400

    Add files via upload

---
+++ ece3849_shared_data/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1294ncpdt.cmd 
+
+C_SRCS += \
+../Crystalfontz128x128_ST7735.c \
+../HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.c \
+../main.c \
+../tm4c1294ncpdt_startup_ccs.c 
+
+C_DEPS += \
+./Crystalfontz128x128_ST7735.d \
+./HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.d \
+./main.d \
+./tm4c1294ncpdt_startup_ccs.d 
+
+OBJS += \
+./Crystalfontz128x128_ST7735.obj \
+./HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.obj \
+./main.obj \
+./tm4c1294ncpdt_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"Crystalfontz128x128_ST7735.obj" \
+"HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.obj" \
+"main.obj" \
+"tm4c1294ncpdt_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"Crystalfontz128x128_ST7735.d" \
+"HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.d" \
+"main.d" \
+"tm4c1294ncpdt_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../Crystalfontz128x128_ST7735.c" \
+"../HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.c" \
+"../main.c" \
+"../tm4c1294ncpdt_startup_ccs.c" 
+
+
