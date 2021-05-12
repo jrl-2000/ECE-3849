c9573f41738a5d9b8d4069a66ddfc16bb8e440ad
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2021-05-12 17:13:14 -0400

    Add files via upload

---
+++ ece3849_profiling/Debug/subdir_vars.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1294ncpdt.cmd 
+
+S_SRCS += \
+../block_copy.s 
+
+C_SRCS += \
+../main.c \
+../tm4c1294ncpdt_startup_ccs.c 
+
+S_DEPS += \
+./block_copy.d 
+
+C_DEPS += \
+./main.d \
+./tm4c1294ncpdt_startup_ccs.d 
+
+OBJS += \
+./block_copy.obj \
+./main.obj \
+./tm4c1294ncpdt_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"block_copy.obj" \
+"main.obj" \
+"tm4c1294ncpdt_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"main.d" \
+"tm4c1294ncpdt_startup_ccs.d" 
+
+S_DEPS__QUOTED += \
+"block_copy.d" 
+
+S_SRCS__QUOTED += \
+"../block_copy.s" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../tm4c1294ncpdt_startup_ccs.c" 
+
+
